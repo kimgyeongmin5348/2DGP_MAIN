2be3e9a44e9622f8d12afc483fa637af8fca6fda
kimgyeongmin5348 <135432080+kimgyeongmin5348@users.noreply.github.com>
2023-10-01 23:59:52 +0900

    Drill 05 제출 (2020182006 김경민)

--- 2DGP-main/Labs/Lecture07_Linear_Movement/move_character_with_random_points.py
+++ 2DGP-main/Labs/Lecture07_Linear_Movement/move_character_with_random_points.py
@@ -15,16 +15,11 @@ hide_cursor()
 
 
 def draw_big_point(p):
-    x = random.randint(0, TUK_WIDTH)
-    y = random.randint(0, TUK_HEIGHT)
-    p[0] = x
-    p[1] = y
-    mouse.draw(p[0], p[1])
-
+    mouse.draw(x,y)
+    pass
 
 def draw_point(p):
-    global frame
-    x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+    frame = 0
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
@@ -33,29 +28,30 @@ def draw_point(p):
 
 
 def move_character(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
+        x1, y1 = p1[0], p1[1]
+        x2, y2 = p2[0], p2[1]
 
-    dx = x2 - x1
-    dy = y2 - y1
+        draw_big_point(p1)
+        draw_big_point(p2)
 
-    draw_big_point(p1)
-    draw_big_point(p2)
-    a = (y2 - y1) / (x2 - x1)
-    b = y1 - x1 * a
+        a = (y2 - y1) / (x2 - x1)
+        b = y1 - x1 * a
+        for j in range(0, 100+1, 4):
+            t = j / 100
+            x = (1 - t) * x1 + t * x2
+            y = (1 - t) * y1 + t * y2
+            draw_point((x,y))
 
-    for i in range(0, 100 + 1, 4):
-        t = i / 100
-        x = (1 - t) * x1 + t * x2
-        y = (1 - t) * y1 + t * y2
-        draw_point((x, y))
+        draw_point(p2)
+        draw_point((x2,y2))
+        pass
+
 
 
 points = [[random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)] for i in range(10)]
 
-for i in range(len(points)):
-    start = points[i]
-    end = points[(i + 1) % len(points)]
-    move_character(start, end)
+for i in range(0,len(points)-1):
+    move_character(points[i], points[i+1])
+move_character(points[-1],points[0])
 
 close_canvas()
